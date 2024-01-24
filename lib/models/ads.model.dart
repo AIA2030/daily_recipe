62146afe6d17de3a960166e444b9cbb70727f362
AIA2030 <132105938+AIA2030@users.noreply.github.com>
2024-01-24 14:22:30 +0200

    Update ads.model.dart

--- lib/models/ads.model.dart
+++ lib/models/ads.model.dart
@@ -1,28 +1,19 @@
 class Ad {
   String? docId;
   String? title;
-  String? subtitle;
+  String? type;
   String? image;
-  String? calories;
-  String? time;
-  String? serving;
+  num? calories;
+  num? time;
+  num? serving;
 
-   Ad();
+  Ad();
 
-  // Ad(
-  //     {required
-  //       this.docId,
-  //       this.title,
-  //       this.subtitle,
-  //       this.image,
-  //       this.serving,
-  //       this.prep_time,
-  //       this.calories});
 
   Ad.fromJson(Map<String, dynamic> data, [String? id]) {
     docId = id;
     title = data['title'];
-    subtitle = data['subtitle'];
+    type = data['type'];
     image = data['image'];
     calories = data['calories'];
     time = data['time'];
@@ -32,7 +23,7 @@ class Ad {
   Map<String, dynamic> toJson() {
     return {
       "title": title,
-      "subtitle": subtitle,
+      "type": type,
       "image": image,
       "calories": calories,
       "time": time,
@@ -40,44 +31,3 @@ class Ad {
     };
   }
 }
-
-// class Ads {
-//   final String title;
-//   final String subtitle;
-//   final String image;
-//   final int calories;
-//   final int prep_time;
-//   final int serving;
-//
-//   String error = '';
-//
-//   Ads({
-//     required this.title,
-//     required this.subtitle,
-//     required this.image,
-//     required this.serving,
-//     required this.calories,
-//     required this.prep_time,
-//   });
-//
-//   factory Ads.fromJson(dynamic json) {
-//     if (json == null) {
-//       return Ads(
-//         title: '',
-//         subtitle: '',
-//         image: '',
-//         calories: 120,
-//         prep_time: 10,
-//         serving: 1,
-//       );
-//     }
-//
-//     return Ads(
-//         title: json['title'],
-//         subtitle: json['subtitle'],
-//         image: json['image'],
-//         calories: json['calories'],
-//         serving: json['serving'],
-//         prep_time: json['prep_time']);
-//   }
-// }
